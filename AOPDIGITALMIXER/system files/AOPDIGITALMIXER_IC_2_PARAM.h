07a118b3fac7eff3101aba53463ed014ae9b8dc7
sonolive <sonolive@wanadoo.fr>
2018-02-08 12:21:27 +0100

    ajouté les repertoires SS

---
+++ AOPDIGITALMIXER/system files/AOPDIGITALMIXER_IC_2_PARAM.h
@@ -0,0 +1,23 @@
+/*
+ * File:           C:\Users\mehdi pailler\Documents\ADAU1452\SKETCHES OLIVES\AOPDIGITALMIXER\system files\AOPDIGITALMIXER_IC_2_PARAM.h
+ *
+ * Created:        Sunday, December 24, 2017 5:54:07 PM
+ * Description:    AOPDIGITALMIXER:IC 2 parameter RAM definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2017 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __AOPDIGITALMIXER_IC_2_PARAM_H__
+#define __AOPDIGITALMIXER_IC_2_PARAM_H__
+
+
+#endif
